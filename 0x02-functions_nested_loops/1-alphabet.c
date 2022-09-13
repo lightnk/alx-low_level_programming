8c98cebb2a24b94e89a1bc286f7d68ff2b109bda
Light <nkemjiis4u@gmail.com>
2022-09-13 06:33:56 -0700

    hot fix 22

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-*print_alphabet_x10 - prints alphabet 10 times 
+*print_alphabet_x10 - prints alphabet 10 times
 *
 *Return: Always 0 (Succes)
 *
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int j;
 
