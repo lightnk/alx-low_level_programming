6b90e57c9dfe856d117b5623e28145b83f29c957
Light <nkemjiis4u@gmail.com>
2022-10-06 03:43:22 -0700

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Readme for 0x0C. C - More malloc, free
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+
+#endif
+
